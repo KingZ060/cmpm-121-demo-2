1625265e9818757467d4f2075dbbdaca7482b40f
Xing <xzhong15@ucsc.edu>
2023-10-23 10:51:31 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,35 @@ canvas.style.border = "thin solid black";
 canvas.style.borderRadius = "10px";
 canvas.style.boxShadow = "5px 5px 10px rgba(0, 0, 0, 0.5)";
 app.append(canvas);
+
+//Step 2
+let isDrawing = false;
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  canvas.getContext("2d")!.beginPath();
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (!isDrawing) return;
+  ctx?.lineTo(e.offsetX, e.offsetY);
+  ctx?.stroke();
+  ctx?.beginPath();
+  ctx?.moveTo(e.offsetX, e.offsetY);
+});
+
+const lineBreak = document.createElement("br");
+app.append(lineBreak);
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+const originX = 0;
+const originY = 0;
+clearButton.addEventListener("click", () => {
+  const ctx = canvas.getContext("2d")!;
+  ctx?.clearRect(originX, originY, canvas.width, canvas.height);
+});
+app.append(clearButton);
