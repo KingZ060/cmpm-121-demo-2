378a156f058cf0e0c1e544b77c2be768cd693625
Xing <xzhong15@ucsc.edu>
2023-10-23 10:34:45 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = " Sticker Sketchpad game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//Step 1
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.border = "thin solid black";
+canvas.style.borderRadius = "10px";
+canvas.style.boxShadow = "5px 5px 10px rgba(0, 0, 0, 0.5)";
+app.append(canvas);
